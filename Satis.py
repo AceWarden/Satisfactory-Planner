c36eb33a07b95ee9f28d03286bf4e84d5469d3f7
AceWarden <120771929+AceWarden@users.noreply.github.com>
2025-11-19 15:19:57 -0500

    Add files via upload

---
+++ Satis.py
@@ -0,0 +1,14 @@
+from Reci import *
+
+#ini
+belt_capacity=60; #default mk1
+resource_limit=belt_capacity; #usually is the case
+
+print(master_list)
+
+#desired output
+des_out=input("What do you want to make?")
+
+#override
+des_out="Iron Plate"
+
